4a78d9d09ed3a1aab21d59b2d1a293413782187e
Rishav1167 <Rishav1167>
2025-06-04 14:09:58 +0530

    [Rishav] INIT : Investment plan microservice

---
+++ investment-plan-ms/src/main/java/com/investmentplanms/InvestmentPlanMsApplication.java
@@ -0,0 +1,13 @@
+package com.investmentplanms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentPlanMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentPlanMsApplication.class, args);
+    }
+
+}
---
+++ investment-plan-ms/src/test/java/com/investmentplanms/InvestmentPlanMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investmentplanms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentPlanMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
