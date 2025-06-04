078fe46ae985a2ce8540e116b8ca02451323b8eb
Rishav1167 <Rishav1167>
2025-06-04 14:09:21 +0530

    [Rishav] INIT : User Profile microservice

---
+++ profile-ms/src/main/java/com/profilems/ProfileMsApplication.java
@@ -0,0 +1,13 @@
+package com.profilems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileMsApplication.class, args);
+    }
+
+}
