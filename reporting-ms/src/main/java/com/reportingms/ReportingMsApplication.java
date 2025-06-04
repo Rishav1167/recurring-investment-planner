be59453a046ea804425bb8fd2a7c3ff9ecde95f5
Rishav1167 <Rishav1167>
2025-06-04 14:20:42 +0530

    [Rishav] INIT : Reporting microservice

---
+++ reporting-ms/src/main/java/com/reportingms/ReportingMsApplication.java
@@ -0,0 +1,13 @@
+package com.reportingms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportingMsApplication.class, args);
+    }
+
+}
