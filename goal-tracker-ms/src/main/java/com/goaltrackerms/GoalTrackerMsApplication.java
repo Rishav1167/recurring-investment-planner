57f9925ff7c71c82fd4cb4a980d4a005b9d59acb
Rishav1167 <Rishav1167>
2025-06-04 14:19:22 +0530

    [Rishav] INIT : Goal Tracker microservice

---
+++ goal-tracker-ms/src/main/java/com/goaltrackerms/GoalTrackerMsApplication.java
@@ -0,0 +1,13 @@
+package com.goaltrackerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalTrackerMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoalTrackerMsApplication.class, args);
+    }
+
+}
---
+++ goal-tracker-ms/src/test/java/com/goaltrackerms/GoalTrackerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goaltrackerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalTrackerMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
